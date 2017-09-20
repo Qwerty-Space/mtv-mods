91ba9703755fffef9794892980111f15b4c19fb4
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2017-09-20 18:39:31 +0100

    Testing....

--- mtv-mods.user.js
+++ mtv-mods.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MTV mods
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      1.1
 // @description  It's in the name
 // @author       Qwerty-Space
 // @match        https://www.morethan.tv/
