b3186580882a9e6670df65f6d4d9a0196f9263b9
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2017-09-20 18:42:04 +0100

    no longer requires p5

--- mtv-mods.user.js
+++ mtv-mods.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         MTV mods
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  It's in the name
 // @author       Qwerty-Space
 // @match        https://www.morethan.tv/
-// @require      http://cdnjs.com/libraries/p5.js
 // ==/UserScript==
 
 var listItem = document.getElementsById("stats_seedpoints")[0];
